e2a2d1e9a03096f727eb7b2cd3a6e0abb50fabd6
aaaadev <laplacianmatrix@gmail.com>
2023-06-07 15:06:45 +0900

    chore: cmake: add binary suffix

---
+++ mingw-w64.cmake
@@ -0,0 +1,10 @@
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
+
+# adjust the default behavior of the FIND_XXX() commands:
+# search programs in the host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+
+# search headers and libraries in the target environment
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
